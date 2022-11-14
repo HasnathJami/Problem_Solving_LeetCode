1865efca7b6564452d61019ed3a87b938ce98881
Hasnath Jami Chowdhury <62906638+HasnathJami@users.noreply.github.com>
2022-11-14 22:50:24 +0600

    Update 217.Contains Duplicate.java

--- Top Interview Questions/JAVA/217.Contains Duplicate.java
+++ Top Interview Questions/JAVA/217.Contains Duplicate.java
@@ -1,4 +1,4 @@
-//Approach 1 (Using Sorted Array) //46ms
+//Approach 1 (Using Sorted Array) //49ms
 // class Solution {
 //     public boolean containsDuplicate(int[] nums) {
 //        Arrays.sort(nums);
@@ -11,25 +11,59 @@
 //     }
 // }
 
-//Appraoch 2 (Using HashMap) //0ms
+//Appraoch 2 (Using HashMap) //74ms
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (hm.containsKey(nums[i]))
+//                 hm.put(nums[i], hm.get(nums[i]) + 1);
+//             else {
+//                 hm.put(nums[i], 1);
+//             }
+//         }
+
+//         for (int i = 0; i < hm.size(); i++) {
+//             if (hm.get(nums[i]) > 1) {
+//                 return true;
+//             }
+//         }
+
+//         return false;
+//     }
+// }
+
+//Approach 3 (Using HashSet) //12ms
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (hm.containsKey(nums[i]))
-                hm.put(nums[i], hm.get(nums[i]) + 1);
-            else {
-                hm.put(nums[i], 1);
-            }
+        HashSet<Integer> hs = new HashSet<Integer>();
+        if (nums == null) {
+            throw new IllegalArgumentException("Input array is null");
         }
-
-        for (int i = 0; i < hm.size(); i++) {
-            if (hm.containsKey(i) && hm.get(i) > 1) {
+        if (nums.length <= 1) {
+            return false;
+        }
+        for (int num : nums) {
+            if (hs.contains(num)) {
                 return true;
             }
+            hs.add(num);
         }
-
         return false;
     }
-}
+ }
+
+//Approach 4 (Using Bruteforce) //TLE
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         for(int i = 0; i < nums.length; i++) {
+//             for(int j = i + 1; j < nums.length; j++) {
+//                 if(nums[i] == nums[j]) {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+//  }
