56be9c87a01a2c4be62348bb99550e09399a5328
Hasnath Jami Chowdhury <62906638+HasnathJami@users.noreply.github.com>
2024-03-04 00:30:22 +0600

    Update 27. Remove Element.java

--- Top Interview Questions/JAVA/27. Remove Element.java
+++ Top Interview Questions/JAVA/27. Remove Element.java
@@ -31,18 +31,37 @@
 // }
 
 // Using Two Pointer (slow and fast pointer)
+// class Solution {
+//     public int removeElement(int[] nums, int val) {
+//         int index = 0;
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] != val) {
+//                // int temp =  nums[index];
+//                 nums[index] = nums[i];
+//                // nums[i] = temp;
+//                 index++;
+//             }
+//         }
+//         return index;
+//     }
+// }
+
 class Solution {
     public int removeElement(int[] nums, int val) {
         int index = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-               // int temp =  nums[index];
-                nums[index] = nums[i];
-               // nums[i] = temp;
+                swap(nums, index, i);
                 index++;
             }
         }
         return index;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
 
