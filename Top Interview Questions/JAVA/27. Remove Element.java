a1236fd89ad75834d0e16aa1c73d78c3bc22fb5c
Hasnath Jami Chowdhury <62906638+HasnathJami@users.noreply.github.com>
2024-03-04 00:26:39 +0600

    Update 27. Remove Element.java

--- Top Interview Questions/JAVA/27. Remove Element.java
+++ Top Interview Questions/JAVA/27. Remove Element.java
@@ -36,7 +36,9 @@ class Solution {
         int index = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
+               // int temp =  nums[index];
                 nums[index] = nums[i];
+               // nums[i] = temp;
                 index++;
             }
         }
